a1a2e79ba640b1c6582d7cf12c6029c904ce6b0a
SteinLundbeckTech <stein@sltech.no>
2025-01-20 12:27:45 +0100

    updated Layout. added design elements to layouts.

--- Program.cs
+++ Program.cs
@@ -2,12 +2,12 @@
     @Date                 : 15.12.2024
     @Author               : Stein Lundbeck
     @Description          : null
-    @Version              : 1.0.0.2
-    @Latest               : 18.12.2024
+    @Version              : 1.0.0.3
+    @Latest               : 20.01.2025
 */
 
 using SLT.Assets.Setup;
 using SLT.Assets.Setup.Feature;
 
 Assets
-    .Setup(new SetupOptions(1, new SSL(), new Debug(), new Route_("SLT", "Index")));
+    .Setup(new SetupOptions(1, new SSL(), new Debug(), new SLT.Assets.Setup.Feature.RouteEntry("SLT", "Index")));
